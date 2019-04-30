977a2b83d12d42674896d379d9f90b1234f0cc3d
Lorenzo <lgdandrea4@gmail.com>
2019-04-29 19:45:47 -0500

    was able to get the console log recording my plus and minuses.  Was also able to tie my numbers specifically to their buttons.  Still working on getting it all to render and count on the DOM

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,62 +1,67 @@
-var targetRandomNumber = Math.floor(Math.random()*100 +1);
-var crystalRandomNumber1 = Math.floor(Math.random()*10 +1);
-var crystalRandomNumber2 = Math.floor(Math.random()*10 +1);
-var crystalRandomNumber3 = Math.floor(Math.random()*10 +1);
-var crystalRandomNumber4 = Math.floor(Math.random()*10 +1);
-var wins = 0;
-var losses = 0;
-var totalNumberGuessed = 0;
-var crystal1 = 0;
-var crystal2 = 0;
-var crystal3 = 0;
-var crystal4 = 0;
-
-crystal1 = crystalRandomNumber1;
-crystal2 = crystalRandomNumber2;
-crystal3 = crystalRandomNumber3;
-crystal4 = crystalRandomNumber4;
-
-
-console.log(crystal1);
-console.log(crystal2);
-console.log(crystal3);
-console.log(crystal4);
-
-
-
-$("#crystal1").on("click", function(){
+$(document).ready(function() {
+    var targetRandomNumber = Math.floor(Math.random()*100 +1);
     var crystalRandomNumber1 = Math.floor(Math.random()*10 +1);
-    console.log(crystalRandomNumber1);
-});
+    var crystalRandomNumber2 = Math.floor(Math.random()*10 +1);
+    var crystalRandomNumber3 = Math.floor(Math.random()*10 +1);
+    var crystalRandomNumber4 = Math.floor(Math.random()*10 +1);
+    var wins = 0;
+    var losses = 0;
+    var totalNumberGuessed = 0;
+    var crystal1 = 0;
+    var crystal2 = 0;
+    var crystal3 = 0;
+    var crystal4 = 0;
+    
+    crystal1 = crystalRandomNumber1;
+    crystal2 = crystalRandomNumber2;
+    crystal3 = crystalRandomNumber3;
+    crystal4 = crystalRandomNumber4;
 
-$("#crystal2").on("click", function(){
+    $("#goal-number").text(targetRandomNumber);
+    $("#losses").text(losses);
+    $("#wins").text(wins);
+    
+    
+    
+    console.log(crystal1);
     console.log(crystal2);
+    console.log(crystal3);
+    console.log(crystal4);
     
-});
-
-$("#crystal3").on("click", function(){
-    var crystalRandomNumber = Math.floor(Math.random()*10 +1);
-    crystal3 = crystalRandomNumber;
-
-});
-
-$("#crystal4").on("click", function(){
-    var crystalRandomNumber = Math.floor(Math.random()*10 +1);
-    crystal4 = crystalRandomNumber;    
-});
-
-    // function endGame() {
-//     if (totalNumberGuessed == targetRandomNumber) {
-//         totalNumberGuessed = 0;
-//         wins++;
-//       run the 4 randomizing crystal functions         
-//     };
-
-//     else if(totalNumberGuessed>targetRandomNumber) {
-//         totalNumberGuessed = 0;
-//         losses++;
-//     };
-//  };
+    $("#crystal1").on("click", function(){
+        totalNumberGuessed = totalNumberGuessed + crystal1;
+        console.log(totalNumberGuessed);
+    });
     
-
-
+    $("#crystal2").on("click", function(){
+        totalNumberGuessed = totalNumberGuessed + crystal2;
+        console.log(totalNumberGuessed);      
+    });
+    
+    $("#crystal3").on("click", function(){
+        totalNumberGuessed = totalNumberGuessed + crystal3;
+        console.log(totalNumberGuessed);
+    });
+    
+    $("#crystal4").on("click", function(){
+        totalNumberGuessed = totalNumberGuessed + crystal4;
+        console.log(totalNumberGuessed);   
+    });
+    
+    // $("#total-number").(totalNumberGuessed)
+    
+    
+    if (totalNumberGuessed == targetRandomNumber) {
+            totalNumberGuessed = 0;
+            wins++;      
+        }
+        
+    else if ( totalNumberGuessed>targetRandomNumber ) {
+            totalNumberGuessed = 0;
+            losses++;
+    };
+})
+                
+                
+                
+                
